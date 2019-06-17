bf3dc8dbb9ed77c5116b6a8f6ec70d05d4fb8cdf
eduardoar <eduardoazvd17@gmail.com>
2019-06-17 12:12:42 -0300

    Implementação do valor total da lista.

--- app/src/main/java/br/net/listacompras/MainActivity.java
+++ app/src/main/java/br/net/listacompras/MainActivity.java
@@ -1,6 +1,8 @@
 package br.net.listacompras;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ProdutosAdapter adapter;
     private List<Produto> produtos;
     private ListaComprasDB db;
+    private double valorTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton btnAdd = findViewById(R.id.btnAdd);
+        ImageButton btnTotal = findViewById(R.id.btnTotal);
         lvProdutos = findViewById(R.id.lvProdutos);
 
         listarProdutos();
@@ -33,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), AdicionarActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnTotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcularValorTotal();
+                mostrarValorTotal();
             }
         });
 
@@ -74,5 +87,25 @@ public class MainActivity extends AppCompatActivity {
         this.produtos = db.getProdutos();
         this.adapter = new ProdutosAdapter(this.produtos, this);
         this.lvProdutos.setAdapter(this.adapter);
+    }
+
+    public void calcularValorTotal() {
+        this.valorTotal = 0;
+        for (Produto produto: produtos) {
+            valorTotal = valorTotal + (produto.getPreco()/100);
+        }
+    }
+
+    public void mostrarValorTotal() {
+        String valorTotalFormatado = NumberFormat.getCurrencyInstance().format(valorTotal);
+        final AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setTitle("Valor Total");
+        dlgAlert.setMessage("Total: " + valorTotalFormatado);
+        dlgAlert.setPositiveButton("Fechar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dlgAlert.create().show();
     }
 }
